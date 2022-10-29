b92ccb43e8e4038f1b7646be2dffb8aaab429d35
Christopher Jones <chris@christopherjones.me>
2022-10-29 14:19:29 -0500

    add themes page

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
+  },
+  {
+    path: 'themes',
+    loadChildren: () => import('./themes/themes.module').then( m => m.ThemesPageModule)
   }
 ];
 
