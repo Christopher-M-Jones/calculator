b258a70b2131c1fabafb217a3f03a01e925b6a56
Christopher Jones <chris@christopherjones.me>
2022-10-29 19:40:47 -0500

    add eventlisteners to theme buttons

--- src/app/home/themes/themes.page.ts
+++ src/app/home/themes/themes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import 'theme/variables.scss';
 
 @Component({
   selector: 'app-themes',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThemesPage implements OnInit {
 
+
+
   constructor() { }
+
+  // changeTheme(theme: string){
+  // --ion-color-cust-primary = '#fff';
+  // --ion-color-cust-secondary: blue;
+  // --ion-color-cust-taskbar: green;
+  // --ion-color-cust-taskbar-border: orange;
+  // }
 
   ngOnInit() {
   }
