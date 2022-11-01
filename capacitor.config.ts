9856ad7dbd118e825c8e1752c448e4f6d636de97
Christopher Jones <chris@christopherjones.me>
2022-11-01 02:57:22 -0500

    add capacitor files

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.cj373.calculator',
   appName: 'calculator',
   webDir: 'www',
   bundledWebRuntime: false
